ec92b643845b8384eab54473621980e0d0087c0f
whyshouldibother <113842019+whyshouldibother@users.noreply.github.com>
2026-05-13 15:02:16 +0545

    Adding logging

--- lib/uploadSvg.ts
+++ lib/uploadSvg.ts
@@ -5,11 +5,16 @@ import {processSvg, assertSvgMimeType} from './svg'
 export async function uploadSvg(
     file: File,
 ): Promise<string> {
+    console.log("Starting Upload");
     assertSvgMimeType(file.type);
 
+    console.log("Reading files");
     const raw = await file.text();
+
+    console.log("Compressing");
     const compressed = processSvg(raw);
 
+    console.log("Inserting");
     const result = await pool.query<{id:string}>("insert into imagesvg (image) values($1) on conflict (hash) do update set hash = excluded.hash returning id", [compressed])
     return result.rows[0].id;
 }
