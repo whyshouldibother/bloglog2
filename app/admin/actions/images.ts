9afa98c1a5553f851bb90626d089e75c9f5499c8
whyshouldibother <113842019+whyshouldibother@users.noreply.github.com>
2026-05-13 14:41:52 +0545

    Log erro

--- app/admin/actions/images.ts
+++ app/admin/actions/images.ts
@@ -4,7 +4,15 @@ import { revalidatePath } from 'next/cache';
 import {uploadSvg} from '@/lib/uploadSvg'
 import {svgType} from '@/types/image'
 export async function uploadCircuit(data:svgType, versionId:number){
+    try{
+
     const imageId = await uploadSvg(data.file);
     await pool.query("insert into circuitimages(imageid, versionid, alt) values($1, $2, $3) on conflict (versionid) do update set imageid = excluded.imageid, alt = excluded.alt ", [imageId, versionId, data.altText]);
     revalidatePath("/admin/projects");
+    }
+    catch(e){
+        console.error("upload circuit fialed:", e);
+        throw e;
+
+    }
 }
